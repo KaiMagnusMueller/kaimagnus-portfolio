d0b3a74f24c9ddf370b9d50cb2df0389f06bcdaf
KaiMagnusMueller <ndsignin@outlook.de>
2023-02-09 21:18:30 +0100

    add video component

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,8 @@ import svelte from "@astrojs/svelte";
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
+
+// https://astro.build/config
 export default defineConfig({
   integrations: [svelte(), mdx()]
 });
