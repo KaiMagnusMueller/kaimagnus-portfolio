dc584f041fc5f05bb7ea782f3fc84cabf3225182
KaiMagnusMueller <ndsignin@outlook.de>
2023-03-06 13:40:56 +0100

    Add links for headings

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,17 +9,26 @@ import mdx from '@astrojs/mdx';
 // https://astro.build/config
 import image from '@astrojs/image';
 
+import rehypeSlug from 'rehype-slug';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import { autolinkConfig } from './plugins/rehype-autolink-config';
+
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://kaimagnus-portfolio-git-dev-kaimagnusmueller.vercel.app/',
 	integrations: [
 		svelte(),
 		mdx(),
+
 		image({
 			serviceEntryPoint: '@astrojs/image/sharp',
 		}),
 	],
 	vite: {
 		plugins: [],
+	},
+
+	markdown: {
+		rehypePlugins: [rehypeSlug, [rehypeAutolinkHeadings, autolinkConfig]],
 	},
 });
