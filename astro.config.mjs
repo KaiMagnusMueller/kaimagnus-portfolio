1d56d99621b6e7afdfe64d08952a076bc0f1e536
Kai Magnus Müller <ndsignin@outlook.de>
2024-02-08 17:59:16 +0100

    Upgrade to Astro v3
    - WIP update images with dimensions

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,6 @@ import svelte from '@astrojs/svelte';
 // https://astro.build/config
 import mdx from '@astrojs/mdx';
 
-// https://astro.build/config
-import image from '@astrojs/image';
-
 import rehypeSlug from 'rehype-slug';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 import { autolinkConfig } from './plugins/rehype-autolink-config';
@@ -20,16 +17,13 @@ export default defineConfig({
 		analytics: true,
 	}),
 	site: 'https://kaimagnus.de/',
-	integrations: [
-		svelte(),
-		mdx(),
-
-		image({
-			serviceEntryPoint: '@astrojs/image/sharp',
-		}),
-	],
+	integrations: [svelte(), mdx()],
 	vite: {
 		plugins: [],
+	},
+
+	image: {
+		remotePatterns: [{ protocol: 'https' }],
 	},
 
 	markdown: {
