e1236a8d07b38ee50551e5373a5051cdc6c1dccf
Kai Magnus Müller <ndsignin@outlook.de>
2024-07-15 18:09:04 +0200

    Added general IDS work post
    - Added general IDS work post
    - Add start and end dates to schema
    - EPIC post text improvements

--- src/content/_schemas.ts
+++ src/content/_schemas.ts
@@ -35,15 +35,17 @@ export type ArticleFrontmatter = z.infer<typeof articleSchema>;
 export const workSchema = z
     .object({
         author: z.string().optional(),
-        datePublished: z.date(),
-        title: z.string(),
-        draft: z.boolean().optional(),
-        featured: z.array(z.string()).default(['']),
-        tags: z.array(z.string()).default(['others']),
-        ogImage: z.string().optional(),
-        description: z.string(),
         customer: z.string().optional(),
+        datePublished: z.date(),
+        description: z.string(),
+        draft: z.boolean().optional(),
         employer: z.string().optional(),
+        featured: z.array(z.string()).default(['']),
+        ogImage: z.string().optional(),
+        projectEndDate: z.date().optional(),
+        projectStartDate: z.date().optional(),
+        tags: z.array(z.string()).default(['others']),
+        title: z.string(),
     })
     .strict();
 
